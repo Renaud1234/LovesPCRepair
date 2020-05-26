234facc8b2194f85b47738eeeae8a9a7952b0089
Renaud Simon <48937051+Renaud1234@users.noreply.github.com>
2020-05-26 17:23:50 +0100

    Timer and visibility

--- function.js
+++ function.js
@@ -13,3 +13,18 @@ function Year(){
     var userAge = todayYear - inputYear;
     document.getElementById('age').value = userAge;
 }
+
+function And1(){
+    document.getElementById('and1').style.display='block';
+    document.getElementById('and2').style.display='none';
+}
+
+function And2(){
+    document.getElementById('and2').style.display='block';
+    document.getElementById('and1').style.display='none';
+}
+
+function showIt(){
+    document.getElementById('timer1').style.visibility="visible";
+}
+setTimeout("showIt();", 3000);
